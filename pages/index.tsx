dd5b9757ade4cc43b57190ddc9906117366c1934
LuximusHunter (Thomas Lipman) <49814847+luximus-hunter@users.noreply.github.com>
2021-03-31 19:45:59 +0200

    convert to TypeScript

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import { Fragment } from "react";
+import Head from "next/head";
+
+// import styles from './Home.module.scss'
+
+const Home = () => {
+  return (
+    <Fragment>
+      <Head>
+        <title>Thomas Lipman</title>
+      </Head>
+      <section>
+        Home
+      </section>
+    </Fragment>
+  );
+};
+
+export default Home;
