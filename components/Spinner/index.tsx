44caa7d2141a9d9a0c6bd0a1d060594aee3affcd
LuximusHunter (Thomas Lipman) <49814847+luximus-hunter@users.noreply.github.com>
2021-04-19 20:57:15 +0200

    clear repo

--- components/Spinner/index.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from 'react';
-import styles from './spinner.module.scss';
-
-interface Props {}
-
-const Spinner: FC<Props> = () => <div className={styles.Spinner} />;
-
-export default Spinner;
