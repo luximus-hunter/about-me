3f171d93c9d702cefc5722d535bf2e59f9ef8726
luximus-hunter <49814847+luximus-hunter@users.noreply.github.com>
2021-05-27 12:08:50 +0200

    lazy load button new way

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,12 @@
 import { FC, Fragment, lazy, useEffect, useState } from 'react';
 
-import Spinner from "../../components/Spinner";
+import Button from '../../components/Button';
+import Spinner from '../../components/Spinner';
 import about from '../../data/about.json';
 import config from '../../config.json';
 import profilePicture from '../../assets/pfp.png';
 import styles from './home.module.scss';
 
-const Button = lazy(() => import('../../components/Button'));
 const Card = lazy(() => import('../../components/Card'));
 const Container = lazy(() => import('../../components/Container'));
 const Flag = lazy(() => import('../../components/Flag'));
@@ -53,39 +53,39 @@ const Home: FC = () => {
 
   return (
     <Container>
-        <Grid centerItems cols={2}>
-          <Grid rows={2}>
-            <Card centerItems>
-              {loading ? (
-                <Spinner />
-              ) : (
-                <img alt='me' src={profilePicture} className={styles.ProfilePicture} />
-              )}
-            </Card>
-            <Card centerItems>
-              <div>
-                <Button onClick={() => changeLang('nl')}>
-                  <Flag className={styles.Flag} lang='nl' />
-                </Button>
-                <Button onClick={() => changeLang('en')}>
-                  <Flag className={styles.Flag} lang='gb' />
-                </Button>
-              </div>
-            </Card>
-          </Grid>
+      <Grid centerItems cols={2}>
+        <Grid rows={2}>
           <Card centerItems>
             {loading ? (
               <Spinner />
             ) : (
-              <Fragment>
-                <h1>{content?.title}</h1>
-                {content?.lines?.map((line, index) => (
-                  <p key={index}>{line}</p>
-                ))}
-              </Fragment>
+              <img alt='me' src={profilePicture} className={styles.ProfilePicture} />
             )}
           </Card>
+          <Card centerItems>
+            <div>
+              <Button onClick={() => changeLang('nl')}>
+                <Flag className={styles.Flag} lang='nl' />
+              </Button>
+              <Button onClick={() => changeLang('en')}>
+                <Flag className={styles.Flag} lang='gb' />
+              </Button>
+            </div>
+          </Card>
         </Grid>
+        <Card centerItems>
+          {loading ? (
+            <Spinner />
+          ) : (
+            <Fragment>
+              <h1>{content?.title}</h1>
+              {content?.lines?.map((line, index) => (
+                <p key={index}>{line}</p>
+              ))}
+            </Fragment>
+          )}
+        </Card>
+      </Grid>
     </Container>
   );
 };
