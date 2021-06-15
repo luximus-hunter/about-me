c5f62fa89d8879ef14ba2caf51b4f3c2f3945d6e
luximus-hunter <49814847+luximus-hunter@users.noreply.github.com>
2021-06-15 15:51:52 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,16 @@ import Home from './pages/Home';
 import Spinner from './components/Spinner';
 import config from './config.json';
 
-const App: FC = () => {
-  let lang: string = navigator.language;
-
-  if (lang === 'en-US' || lang === 'en-GB') {
-    lang = 'en';
-  }
-
-  if (!localStorage.getItem('lang')) {
-    localStorage.setItem('lang', lang);
-  }
-
-  return (
-    <Suspense fallback={Spinner}>
-      <Router>
-        <Switch>
-          <Route path='/' exact render={() => <Redirect to={'/' + config.defaultLanguage} />} />
-          <Route path='/:lang' exact component={Home} />
-          <Redirect to='/' />
-        </Switch>
-      </Router>
-    </Suspense>
-  );
-};
+const App: FC = () => (
+  <Suspense fallback={Spinner}>
+    <Router>
+      <Switch>
+        <Route path='/' exact render={() => <Redirect to={'/' + config.defaultLanguage} />} />
+        <Route path='/:lang' exact component={Home} />
+        <Redirect to='/' />
+      </Switch>
+    </Router>
+  </Suspense>
+);
 
 export default App;
