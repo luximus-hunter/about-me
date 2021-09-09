42bd2245dc6d4ba864d82915647edd700fc80323
luximus-hunter <49814847+luximus-hunter@users.noreply.github.com>
2021-09-09 13:35:44 +0200

    Updates

--- src/components/Spinner/index.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from 'react';
-import styles from './spinner.module.scss';
-
-interface Props {}
-
-const Spinner: FC<Props> = () => <div className={styles.spinner} />;
-
-export default Spinner;
