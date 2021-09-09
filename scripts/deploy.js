1c32cb1e2c7a297fef56424fda42f53d70dc0809
luximus-hunter <49814847+luximus-hunter@users.noreply.github.com>
2021-09-09 14:58:45 +0200

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,20 +4,20 @@ const fs = require('fs');
 (async () => {
   try {
     await execa('git', ['checkout', '--orphan', 'live']);
-    console.log('Formatting...');
+    console.log('📃 Formatting');
     await execa('npm', ['run', 'lint']);
-    console.log('Building...');
+    console.log('🚧 Building');
     await execa('npm', ['run', 'build']);
     // Understand if it's dist or build folder
     const folderName = fs.existsSync('dist') ? 'dist' : 'build';
     await execa('git', ['--work-tree', folderName, 'add', '--all']);
     await execa('git', ['--work-tree', folderName, 'commit', '-m', 'live']);
-    console.log('Pushing to live...');
+    console.log('🎈 Pushing to live');
     await execa('git', ['push', 'origin', 'HEAD:live', '--force']);
     // await execa("rm", ["-r", folderName]);
     await execa('git', ['checkout', '-f', 'master']);
     await execa('git', ['branch', '-D', 'live']);
-    console.log('Successfully deployed');
+    console.log('🚀 Successfully deployed');
   } catch (e) {
     console.log(e.message);
     process.exit(1);
