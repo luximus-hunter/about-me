f33118ce9a109ae2769de0a8617687c8f1dc39ec
Luximus <thomaslipman10@gmail.com>
2021-09-28 16:29:32 +0200

    add footer

---
+++ src/sections/Footer/index.tsx
@@ -0,0 +1,9 @@
+import styles from './footer.module.scss';
+
+const Footer = () => (
+    <footer className={styles.footer}>
+        <h1 className={styles.title}>&copy; Thomas Lipman</h1>
+    </footer>
+);
+
+export default Footer;
