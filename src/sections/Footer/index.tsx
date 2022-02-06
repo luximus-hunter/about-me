d5b198031b0df2ef1cf4dea306c97721bed9d062
luximus-hunter <luximus-hunter@users.noreply.github.com>
2022-02-06 15:10:55 +0000

    📝 (format code)

--- src/sections/Footer/index.tsx
+++ src/sections/Footer/index.tsx
@@ -1,12 +1,14 @@
 import styles from './footer.module.scss';
 
 const Footer = () => {
-    const startYear = "2021"
+    const startYear = '2021';
     const currentYear = new Date().getFullYear();
 
     return (
         <footer className={styles.footer}>
-            <h1 className={styles.title}>&copy; Thomas Lipman, {startYear} - {currentYear}</h1>
+            <h1 className={styles.title}>
+                &copy; Thomas Lipman, {startYear} - {currentYear}
+            </h1>
         </footer>
     );
 };
